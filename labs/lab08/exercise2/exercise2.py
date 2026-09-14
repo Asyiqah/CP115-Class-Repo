fe3b7d144227a5acadfc07f2a66d144dbbc45ee4
Luqmanbrats <fannyskylark1659@gmail.com>
2026-09-14 09:36:38 +0800

    changes in lab 8

--- labs/lab08/exercise2/exercise2.py
+++ labs/lab08/exercise2/exercise2.py
@@ -3,6 +3,31 @@ base_salary = float(input())
 overtime_hours = int(input())
 tax_status = input()
 
+tax_rate = 0.0
+overtime_pay = overtime_hours * 35
+gross = base_salary + overtime_pay
+
+if tax_status == "single":
+    if gross >= 5000:
+        tax_rate = 0.22
+    else:
+        tax_rate = 0.18
+elif tax_status == "married":
+    if gross >= 6000:
+        tax_rate = 0.20
+    else:
+        tax_rate = 0.15
+else:
+    if gross >= 5500:
+        tax_rate = 0.25
+    else:
+        tax_rate = 0.19
+
+income_tax = gross * tax_rate
+epf = gross * 0.11
+socso = gross * 0.005
+
+net_salary = gross - income_tax - epf - socso
 
 
 print(employee_name)
